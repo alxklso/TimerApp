da0f52277d13bce0cf26e6f744778141264de19c
alxklso <oslekxela@gmail.com>
2025-08-02 01:01:14 -0400

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by AK on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
